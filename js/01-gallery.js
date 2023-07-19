2f0f491c45336805def987dd33bb6a89896c75be
KaterinichViktor <vspasedust@gmail.com>
2023-07-19 13:29:09 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import * as basicLightbox from 'basiclightbox';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+gallery.innerHTML = galleryItemsMarkup;
+
+const onGalleryItemClick = event => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const largeImageUrl = event.target.dataset.source;
+    openModal(largeImageUrl);
+  }
+};
+
+gallery.addEventListener('click', onGalleryItemClick);
+
+const openModal = largeImageUrl => {
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+  `);
+  instance.show();
+
+  window.addEventListener('keydown', onModalWindowKeydown);
+
+  function onModalWindowKeydown(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onModalWindowKeydown);
+    }
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+  
+  const gallery = document.querySelector('.gallery');
+  
+  const createGalleryItemMarkup = ({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  };
+  
+  const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+  gallery.innerHTML = galleryItemsMarkup;
+  
+  script.onload = initializeLightbox;
 
-console.log(galleryItems);
+  
+//   // Ініціалізація бібліотеки SimpleLightbox після завантаження скрипту
+//   function initializeLightbox() {
+//     const lightbox = new SimpleLightbox('.gallery a', {
+//       captions: true,
+//       captionDelay: 250,
+//       captionPosition: 'bottom',
+//       captionType: 'attr',
+//       captionSrc: 'alt',
+//     });
+//   }
+
+// console.log(galleryItems);
