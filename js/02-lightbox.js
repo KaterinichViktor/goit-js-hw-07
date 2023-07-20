b7802fb500a2a3836f13b3f0178aa33553d18102
KaterinichViktor <vspasedust@gmail.com>
2023-07-20 12:25:42 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,37 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-  
-  const gallery = document.querySelector('.gallery');
-  
-  const createGalleryItemMarkup = ({ preview, original, description }) => {
-    return `
-      <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            alt="${description}"
-          />
-        </a>
-      </li>
-    `;
-  };
-  
-  const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkup).join('');
-  gallery.innerHTML = galleryItemsMarkup;
-  
-  script.onload = initializeLightbox;
 
-  
-//   // Ініціалізація бібліотеки SimpleLightbox після завантаження скрипту
-//   function initializeLightbox() {
-//     const lightbox = new SimpleLightbox('.gallery a', {
-//       captions: true,
-//       captionDelay: 250,
-//       captionPosition: 'bottom',
-//       captionType: 'attr',
-//       captionSrc: 'alt',
-//     });
-//   }
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
 
-// console.log(galleryItems);
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true, 
+  captionSelector: 'img', 
+  captionType: 'attr', 
+  captionsData: 'alt',
+  captionDelay: 250, 
+});
